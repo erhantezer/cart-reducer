4af9b6f7e80b434a4dfed604249cb4657b613e91
Erhan <erhantezer68@gmail.com>
2023-04-21 20:41:45 +0300

    added api

--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import reducer from './reducer';
+import cartItems from "./helper/data";
 
 const AppContext = createContext();
 
@@ -6,11 +8,15 @@ export const useGlobalContext = () => {
     return useContext(AppContext);
 };
 
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
 export const AppProvider = ({children}) => {
+    const [loading, setLoading] = useState(false);
 
     return (
         <AppContext.Provider>
             {children}
         </AppContext.Provider>
         )
-}
+};
