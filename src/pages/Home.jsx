c208e0e29e311b1c0bbf3295ee2b1714754f4ac7
Erhan <erhantezer68@gmail.com>
2023-04-21 19:08:40 +0300

    updated project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import CarContainer from '../components/cart/CarContainer'
+
+const Home = () => {
+    return (
+        <main>
+            <Navbar/>
+            <CarContainer/>
+        </main>
+    )
+}
+
+export default Home
