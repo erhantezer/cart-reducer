70e4a96f61c76aabc97f87bdc2b80df97695bdfa
Erhan <erhantezer68@gmail.com>
2023-04-21 22:09:18 +0300

    updated context

--- src/context.jsx
+++ src/context.jsx
@@ -1,6 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 import reducer from './reducer';
 import cartItems from "./helper/data";
+import axios from "axios";
 
 const AppContext = createContext();
 
@@ -8,14 +9,32 @@ export const useGlobalContext = () => {
     return useContext(AppContext);
 };
 
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+}
+
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
 export const AppProvider = ({children}) => {
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const fetchData = async () => {
+        dispatch({type: "LOADING"});
+        const {data} = await axios(url)
+        dispatch({type: "DISPLAY_ITEMS", payload: data})
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={{}}>
             {children}
         </AppContext.Provider>
         )
