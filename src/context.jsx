f53d484e4011197ed2fbdac20580b50f0a0bea09
Erhan <erhantezer68@gmail.com>
2023-04-23 12:53:03 +0300

    context  added function dispatch

--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from './reducer';
 import cartItems from "./helper/data";
 import axios from "axios";
@@ -9,6 +9,7 @@ export const useGlobalContext = () => {
     return useContext(AppContext);
 };
 
+
 const initialState = {
     loading: false,
     cart: cartItems,
@@ -16,17 +17,40 @@ const initialState = {
     amount: 0,
 }
 
-
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
-export const AppProvider = ({children}) => {
+export const AppProvider = ({ children }) => {
     // const [loading, setLoading] = useState(false);
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const clearCart = () => {
+        dispatch({ type: "CLEAR_CART" })
+    };
+
+    const remove = (id) => {
+        dispatch({ type: "REMOVE_CART", payload: id })
+    };
+
+    const increase = (id) => {
+        dispatch({ type: "INCREASE", payload: id })
+    };
+
+    const decrease = (id) => {
+        dispatch({ type: "DECREASE", payload: id })
+    };
+
+    const toogleAmount = (id, type) => {
+        dispatch({ type: "TOOGLE_AMOUNT", payload: { id, type } })
+    };
+
+    useEffect(() => {
+        dispatch({ type: "GET_TOTALS" })
+    }, [state.cart]);
+
     const fetchData = async () => {
-        dispatch({type: "LOADING"});
-        const {data} = await axios(url)
-        dispatch({type: "DISPLAY_ITEMS", payload: data})
+        dispatch({ type: "LOADING" });
+        const { data } = await axios(url)
+        dispatch({ type: "DISPLAY_ITEMS", payload: data })
     };
 
     useEffect(() => {
@@ -34,8 +58,15 @@ export const AppProvider = ({children}) => {
     }, [])
 
     return (
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={{
+            ...state,
+            clearCart,
+            remove,
+            increase,
+            decrease,
+            toogleAmount,
+        }}>
             {children}
         </AppContext.Provider>
-        )
+    )
 };
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,4 +1,4 @@
-const initialState = {}
+
 
 export default (state, action) => {
     
