4f7d6f1dea089995a9044f2c3382e4533f7bd489
Erhan <erhantezer68@gmail.com>
2023-04-23 19:38:09 +0300

    updated cart item

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -2,7 +2,19 @@
 
 const CartItem = () => {
     return (
-        <div>CartItem</div>
+        <article className="cart-item">
+            <img src="" alt="" />
+            <div>
+                <h4></h4>
+                <h4 className="item-price"></h4>
+                <button className="remove-btn">
+                    remove
+                </button>
+            </div>
+            <div>
+                
+            </div>
+        </article>
     )
 }
 
