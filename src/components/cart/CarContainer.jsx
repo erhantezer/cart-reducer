343264c7eb08c4a2e05450a842f667992d93a025
Erhan <erhantezer68@gmail.com>
2023-04-24 23:21:22 +0300

    cart edited

--- src/components/cart/CarContainer.jsx
+++ src/components/cart/CarContainer.jsx
@@ -3,12 +3,17 @@ import CartItem from "./CartItem"
 
 
 const CarContainer = () => {
-    const {cart, total, clearCart} = useGlobalContext()
+    const { cart, total, clearCart } = useGlobalContext()
 
     if (cart.length === 0) {
         return (
-            
-            )
+            <section className='cart'>
+                <header>
+                    <h2>your bag</h2>
+                    <h4 className='empty-cart'>is currently empty</h4>
+                </header>
+            </section>
+        )
     }
 
     return (
@@ -17,13 +22,15 @@ const CarContainer = () => {
                 <h2>your bag</h2>
             </header>
             <div>
-                <CartItem />
+                {cart.map((item) => {
+                    return <CartItem key={item.id} {...item} />
+                })}
             </div>
             <footer>
                 <hr />
                 <div className="cart-total">
                     <h4>
-                        total <span></span>
+                        total <span>${total}</span>
                     </h4>
                 </div>
                 <button className="btn clear-btn">
