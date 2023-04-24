22847d84e7799e8a6de44f89e68219961e0e47f4
Erhan <erhantezer68@gmail.com>
2023-04-24 11:10:07 +0300

    reducer updated

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -4,5 +4,9 @@ export default (state, action) => {
     
     if (action.type === "LOADING") {
         return { ...state, loading: true }
+    };
+
+    if (action.type === "CLEAR_CART") {
+        return { ...state, total: 0, amount: 0, loading: false, cart:[] }
     }
 }
